0c002736e475fb25f5aad8cf9e18461f3659f4f9
sudhip kumar <123609652+sudhipkumar@users.noreply.github.com>
2025-03-25 19:24:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 var isDate = function (input) {
   //   write your code here
+	
  // Check if the input is a Date object and is valid
+	if (input instanceof Date && !isNaN(input.getTime())) {
+    return true;
+  }
 };
 
 // Do not change the code below.
